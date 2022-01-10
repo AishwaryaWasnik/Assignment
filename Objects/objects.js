ac9036c4cc6426322c7bef6cc43e64f0af8dc266
AishwaryaWasnik <aishwarya.wasnik@perficient.com>
2022-01-10 17:43:27 +0530

    Assignment about object creation and priniting.

---
+++ Objects/objects.js
@@ -0,0 +1,20 @@
+let Person ={
+    FirstName: "Aishwarya",
+    LastName: "Wasnik",
+  EmailID: "aishwarya.wasnik@perficient.com",
+  PhoneNo: 7558399408,
+  Age: 22,
+  Designation: "Intern Consulting",
+  address: {
+    HouseNo: 396/12,
+    StreetName: "Angulimal Nazul Layout",
+    Locality: "Harizon colony Bezonbagh",
+    CityName: "Nagpur",
+    CountryName: "India",
+},
+};
+console.log(Person);
+
+console.log("Person Name: " + Person["FirstName"] +" "+ Person["LastName"]);
+console.log("Person PhoneNo: " + Person["PhoneNo"]);
+
